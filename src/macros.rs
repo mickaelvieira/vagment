44b099cbeb4c01045bc8ec889c08714e74f61bf7
Mickael Vieira <contact@mickael-vieira.com>
2017-03-03 13:22:10 +0000

    Add missing command

--- src/macros.rs
+++ src/macros.rs
@@ -4,6 +4,7 @@ macro_rules! list_commands {
         let mut c:Vec<&str> = Vec::new();
         c.push("up");
         c.push("halt");
+        c.push("ssh");
         c.push("destroy");
         c.push("status");
         c.push("suspend");
