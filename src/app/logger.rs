3f14d8fa3fadba5f91e1996a32b7acbaf51a73f7
Mickael Vieira <contact@mickael-vieira.com>
2017-06-14 13:30:28 +0100

    Remove dead code

--- src/app/logger.rs
+++ src/app/logger.rs
@@ -12,11 +12,3 @@ pub fn error<S>(message: S)
 {
     println!("{}", Red.paint(message.into()));
 }
-
-pub fn log_result(result: Result<String, String>) {
-    if result.is_err() {
-        error(result.unwrap_err());
-    } else {
-        info(result.unwrap());
-    }
-}
