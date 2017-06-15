0d33cd78e84849ba300c7172cde8340664909100
Mickael Vieira <contact@mickael-vieira.com>
2017-06-15 12:17:07 +0100

    Improve logger output

--- src/app/logger.rs
+++ src/app/logger.rs
@@ -1,14 +1,21 @@
 use ansi_term::Colour::Red;
 use ansi_term::Colour::Green;
+use ansi_term::Style;
 
 pub fn info<S>(message: S)
     where S: Into<String>
 {
-    println!("{}", Green.paint(message.into()));
+    let message = message.into();
+    if !message.is_empty() {
+        println!("{} {}", Style::new().bold().fg(Green).paint("Info:"), message);
+    }
 }
 
 pub fn error<S>(message: S)
     where S: Into<String>
 {
-    println!("{}", Red.paint(message.into()));
+    let message = message.into();
+    if !message.is_empty() {
+        println!("{} {}", Style::new().bold().fg(Red).paint("Error:"), message);
+    }
 }
